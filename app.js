0b7baf14eb9a1abd816527d01e630ae833835105
HuongNTN <huongntn@mecorp.vn>
2019-09-22 11:30:39 +0700

    body parser

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 // npm install express ejs
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 app.listen(3000, () => console.log('Server start!'))
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
     let username = 'Admin'
     res.render('home', { username })
 })
 app.get('/login', (req, res) => {
     res.render('login')
+})
+
+app.post('/login', (req, res) => {
+
 })
