c9bc0a1a0c6a7338b152043829202ecc6a6e974e
HuongNTN <huongntn@mecorp.vn>
2019-09-22 10:46:25 +0700

    /

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Server start!'))
 app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    res.render('home')
+})
