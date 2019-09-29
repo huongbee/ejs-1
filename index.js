e954866d66dc5ddc145dedac927409e63ae6bdb0
HuongNTN <huongntn@mecorp.vn>
2019-09-29 09:46:11 +0700

    list singer

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { Singer, profileLink, arraySinger, arraySinger } = require('./lib/Singer');
+const { Singer, profileLink, avatarLink, arraySinger } = require('./lib/Singer');
 
 app.listen(3000, () => console.log('Server start!'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.render('singer/index');
+    res.render('singer/index', { arraySinger, avatarLink, profileLink });
 })
