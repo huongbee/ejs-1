a1d81d2a757080401f4ca9e7d8f42024e1747aa9
HuongNTN <huongntn@mecorp.vn>
2019-09-29 09:16:03 +0700

    module.exports

---
+++ index.js
@@ -0,0 +1,14 @@
+// npm init --y
+// npm install express ejs
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { Singer, profileLink, arraySinger, arraySinger } = require('./lib/Singer');
+
+app.listen(3000, () => console.log('Server start!'))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('singer/index');
+})
