bf92f5f7c85aa770369aa3ee612b8a005497b70a
HuongNTN <huongntn@mecorp.vn>
2019-09-29 11:53:57 +0700

    update

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.post('/add', (req, res) => {
     res.redirect('/')
 })
 
+app.post('/add', (req, res) => {
+    const { name, link, avatar } = req.body;
+    const _id = Math.ceil(Math.random() * 1000);
+    const singer = new Singer(_id, name, link, avatar);
+    arraySinger.push(singer);
+    res.redirect('/')
+})
+
 app.get('/update/:id', (req, res) => {
     const id = req.params.id;
     const singer = arraySinger.find(s => s._id == id);
@@ -31,10 +39,18 @@ app.get('/update/:id', (req, res) => {
     }
     res.render('singer/update', { singer });
 })
-app.post('/add', (req, res) => {
-    const { name, link, avatar } = req.body;
-    const _id = Math.ceil(Math.random() * 1000);
-    const singer = new Singer(_id, name, link, avatar);
-    arraySinger.push(singer);
+
+app.post('/update/:id', (req, res) => {
+    const id = req.params.id;
+    const { name, avatar, link } = req.body;
+    const index = arraySinger.findIndex(s => s._id == id);
+    if (index < 0) { // ko tim thay singer
+        return res.send(404);
+    }
+    // const singer = new Singer(id, name, link, avatar);
+    // arraySinger[index] = singer
+    arraySinger[index].name = name;
+    arraySinger[index].avatar = avatar;
+    arraySinger[index].link = link;
     res.redirect('/')
 })
